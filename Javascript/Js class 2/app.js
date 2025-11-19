9e550624fa5f61e30ffa88b15e410888fa81f5f1
Majid Hussain <majidrajput246@gmail.com>
2025-11-19 19:12:22 +0500

    css class 14 and js class 2

---
+++ Javascript/Js class 2/app.js
@@ -0,0 +1,31 @@
+
+// var myName = camel case 
+// var MyName = pascal case
+// myname kabab case
+
+var name1 = 'Saylani';
+var num1 = 12;
+var num2 = 10;
+var num3 = '11';
+var num4 = '11';
+var greeting = 'assalam wa alaekum'
+
+// document.write(name1 + '<br>');
+// document.write(num1);
+
+// document.write( greeting + ' ' + 'Majid' );
+
+// document.write(num1 + num2 + '<br>')
+// document.write(num3 + num4 + '<br>')
+// document.write(num3 - num1)
+// document.write(num1 * num3 + '<br>')
+// document.write(num1 / num4 + '<br>')
+// document.write(num3 * num4)
+
+var originalNum = "23";
+document.write(originalNum)
+
+
+ var newNum = originalNum + 7;
+ document.write(newNum)
+

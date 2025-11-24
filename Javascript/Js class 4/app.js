92236450bd09bcb10ec62874ab2d6fce5e980ad4
Majid Hussain <majidrajput246@gmail.com>
2025-11-24 18:57:50 +0500

    js class 4

---
+++ Javascript/Js class 4/app.js
@@ -0,0 +1,40 @@
+
+// var totalCost1 = 1 + (3 * 4);
+// var totalCost2 = (1 + 3) * 4;
+
+// document.write('totalCost1 : ',totalCost1 + '<br>')
+// document.write('totalCost2 : ',totalCost2)
+
+// var name = prompt('enter Your Name');
+// var phone = prompt('your number')
+
+// console.log(name);
+
+// var num1 = prompt('enter first number')
+// var num2 = prompt('enter second number')
+
+// document.write(Number(num1) + Number(num2))
+
+// if else
+
+// var x = prompt("Where does the Pope live?");
+//   if (x === "Vatican") {
+//    alert("Correct!");
+//  }    
+
+// var x = prompt("Where does the Pope live?");
+//  if(x === "Vatican"){
+//     alert("Correct! answer")
+//  }else{
+//     alert('Wrong answer')
+//  }
+
+var x = 10;
+
+// if(x == '10'){
+//     document.write('yes')
+// }
+if(x === '10'){
+    document.write('yes')
+}
+ 

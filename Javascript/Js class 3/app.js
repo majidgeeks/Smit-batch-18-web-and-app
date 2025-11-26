609d122a690419315bc03d5d2ae688d69f91a6f9
Majid Hussain <majidrajput246@gmail.com>
2025-11-26 19:33:26 +0500

    just introduce

---
+++ Javascript/Js class 3/app.js
@@ -0,0 +1,35 @@
+// var myName2;
+// var num1_
+
+// var firstName = "new ";
+// var lastName = 'world';
+// var num1 = '1';
+// var num2 = '2';
+
+
+// document.write( firstName + lastName ) 
+// console.log(firstName+lastName)
+// // console.log(num1)
+// var student1 = 12;
+
+// var student2 = 15;
+
+// var newTotal = student1 + student2
+
+// document.write(newTotal)
+
+// var num1 = 3 % 2;
+
+// document.write(num1)
+
+// var num1 = 10;
+
+// // num1++;
+// num1--;
+
+// document.write(num1)
+
+ var num = 1;
+var newNum = ++num;
+document.write(num)
+
---
+++ Javascript/Js class 5/app.js
@@ -0,0 +1,27 @@
+// if else
+
+// let isHungry = true;
+
+// if (isHungry) {
+//   console.log(" Let's eat!");
+// } else {
+//   console.log(" Maybe later...");
+// }    
+
+// primitive data types
+// 1. 'string'
+// 2. number
+// 3.undefined
+// 4.null 
+
+// Non prmitive data types 
+
+var myName = undefined
+var myName = null
+let name1 = undefined
+name1 = ''
+var name3 = 0
+const war = 'america'
+// war = 12;
+let myWorld;
+myWorld = 'it is'

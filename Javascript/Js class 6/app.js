d8fdd147e7843f56d17370eb665ab87658714c4a
Majid Hussain <majidrajput246@gmail.com>
2025-12-03 19:23:05 +0500

    js class 6 if else

---
+++ Javascript/Js class 6/app.js
@@ -0,0 +1,84 @@
+// if else
+
+// let isHungry = true;
+
+// if (isHungry) {
+//   console.log(" Let's eat!");
+// } else {
+//   console.log(" Maybe later...");
+// }    
+
+// primitive data types
+// 1. 'string'
+// 2. number
+// 3.undefined
+// 4.null 
+//    5. boolean = true / false
+
+// Non prmitive data types 
+
+// var myName = undefined
+// var myName = null
+// let name1 = undefined
+// name1 = ''
+// var name3 = 0
+// const war = 'america'
+// // let myWorld;
+// // myWorld = 'it is'
+
+// // if(war == 'america'){
+    
+// // }
+// var war2 = 12;
+
+// if(war2 === 10){
+//    alert('yes it is ')
+// }
+// else{
+//     alert('not')
+// }
+
+//   var player = prompt('from which city you are');
+//   var score = 0;
+//   var userIQ = 'not available'
+//   var city = "karachi";
+
+//   if(player === city){
+//     score++;
+//     userIQ = "genius";
+//     document.write(userIQ + '<br>') ;
+//     document.write(score);
+//     alert('you can come')
+//   }
+//   else{
+//     alert('you are not allowed')
+//   }
+
+//   if (x === correctAnswer) {
+//     score++;
+//     userIQ = "genius";
+//     alert("Correct!");
+//   }
+
+
+// (x + y === a - b)
+
+// var x = 10;
+// var y = 10;
+// var a = 40;
+// var b = 40;
+
+// if(x + y !== a - b){
+//     alert('yes it is false');
+// }
+
+
+var age = 30;
+var correction = 'Age is correct';
+var notCorrect = 'Your age is wrong';
+
+if(age < 20){
+   alert(correction);
+}else{
+   alert(notCorrect);
+}

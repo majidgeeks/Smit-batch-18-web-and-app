9b2e53d39d2c30ce52b56c2a43224008940d631e
Majid Hussain <majidrajput246@gmail.com>
2025-12-09 13:18:17 +0500

    js class 9

---
+++ Javascript/Js class 9/app.js
@@ -0,0 +1,70 @@
+// var weight = 1000;
+// var time = 10;
+
+// if(weight > 700 && time < 12){
+//    document.write('<h1>Yesss its right</h1>')
+// }else{
+//     document.write('No its wrong');
+// }
+
+// if(weight > 700 && time > 12){
+//    document.write('<h1>Yesss its right</h1>')
+// }else{
+//     document.write('No its wrong');
+// }
+
+// if(weight > 700 || time > 12){
+//    document.write('<h1>Yesss its right</h1>')
+// }else{
+//     document.write('No its wrong');
+// }
+
+
+// Arrays
+
+// var myData = []; 
+// document.write(typeof myData)
+
+// data types 
+
+// primitive data types 
+
+// 1.Number
+// 2. string 
+// 3. boolean
+// 4. undefined
+// 5. null
+
+// non primitive data types 
+
+// 1. Array 
+// 2. object 
+
+// var str = '';
+
+// document.write(typeof str)
+
+// var isAvaiable = true
+// document.write(typeof isAvaiable)
+
+
+// var myData = []; 
+// var myName = ''
+// document.write(Array.isArray(myName));
+
+var arr1 = [12, 13, 15, 20, "yellow", 'Saylani', true, undefined, null]
+
+// document.write(arr1.length)
+
+// document.write(arr1[5]+'<br>');
+// document.write(arr1[7]);
+
+document.write('before', arr1 + '<br>')
+
+var index2 = 100;
+
+arr1[2] = index2
+
+document.write('after',arr1)
+
+

cbd41df4e0477c5836797e6f42cde95383184271
Majid Hussain <majidrajput246@gmail.com>
2025-12-11 12:33:04 +0500

    js class 10 array

---
+++ Javascript/Js class 10/app.js
@@ -0,0 +1,36 @@
+// var friuts = ['banana', 'mango', 'apple'];
+
+// console.log('before pop',friuts)
+
+// friuts.pop();
+
+// console.log('after pop',friuts);
+
+// friuts.push('pear', 'kiwi', 'orange')
+
+// console.log('after push 3 friuts', friuts);
+
+
+// var friuts = ['banana', 'mango', 'apple'];
+
+// console.log('123', friuts);
+
+// friuts.shift();
+
+// console.log('after shift', friuts);
+
+// friuts.unshift('strawberry', 'avocado')
+
+
+// var friuts = ['banana', 'mango', 'apple'];
+
+// friuts.splice(1,1,'lychee','pineapple')
+
+// console.log('after',friuts)
+
+
+var friuts = ['banana', 'mango', 'apple'];
+
+
+
+

194a2b671b14cefe2bcebef41e5ecc220e991063
Majid Hussain <majidrajput246@gmail.com>
2025-12-19 23:02:38 +0500

    js class 14 loops

---
+++ Javascript/Js class 14/app.js
@@ -0,0 +1,21 @@
+var cities = ['Karachi', 'Islamabad', 'Lahore', 'Multan', 'Murree', 'Swat'];
+
+var userInput = prompt('enter city name');
+var isValid = false;
+
+for(var i = 0; i < cities.length; i++){
+ if(userInput == cities[i]){ 
+    alert('you are eligible');
+    isValid = true;
+    break;
+}
+};
+
+if(userInput != cities[i]){
+    alert('you are not eligible');
+    isValid = false;
+}
+
+
+
+console.log('isValid',isValid)

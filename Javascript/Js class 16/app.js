b505496987a6faba36e2e16afb6fe883c7ae0518
Majid Hussain <majidrajput246@gmail.com>
2025-12-24 20:52:54 +0500

    changing case

---
+++ Javascript/Js class 16/app.js
@@ -0,0 +1,34 @@
+// var cleanestCities = ["Cheyenne", "cheyenne", "Santa Fe", "santa fe", "Tucson", "tucson", "Great", "Falls","great falls", "Honolulu", "honolulu"]
+
+// // var myName = 'majid';
+// var myName = 'Majid';
+// // var chageCase = myName.toLowerCase();
+
+// var userInput = prompt('enter the name');
+
+// //  var chageCase = userInput.toUpperCase(); 
+// var firstLetter = userInput[0].toUpperCase();
+// var remainLetters = userInput.slice(1).toLowerCase();
+
+// // console.log('firstLetter',firstLetter)
+// // console.log('remainLetters',remainLetters);
+
+// var concatenate = firstLetter + remainLetters;
+
+//  if(myName === chageCase){
+//     alert('yes')
+//  }else{
+//     alert('No');
+//  }
+
+// if(myName === concatenate){
+//     alert('yes')
+//  }else{
+//     alert('No');
+//  }
+
+
+// var names = ['wajid', 'mohsin', 'waheed'];
+
+
+

9d1b37a3596a6eacc62c908e075973bafa5eb5ce
Majid Hussain <majidrajput246@gmail.com>
2025-12-26 19:04:25 +0500

    js class 17

---
+++ Javascript/Js class 17/app.js
@@ -0,0 +1,60 @@
+// finding string 
+
+// var str1 = 'The name hammad';
+
+// // for(var i = 0; i < str1.length; i++){
+// //     console.log(str1[i]);
+// // }
+
+// var find = indexOf()
+
+// var arr1 = ['mohib', 'rehan', 'shehzad'];
+
+// console.log(arr1.indexOf('shehzad'))
+
+// console.log(arr1.lastIndexOf('mohib'))
+
+// var str = 'The qiuck fox Jumps on lazy dog';
+
+// var finding = str.indexOf('J');
+
+// console.log(finding)
+
+// var arr1 = ['mohib', 'rehan', 'shehzad'];
+
+// console.log(arr1.indexOf('fahad'))
+
+// var text = 'just imagin it was World War II'
+
+//  for (var i = 0; i < text.length; i++) {
+//    if (text.slice(i, i + 12) === "World War II") {
+     
+//       text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//   }
+//  }
+
+//  console.log(text)
+
+
+// var text = 'just imagin it was World War II';
+
+// var change = text.replace('imagin', 'image');
+
+// console.log(change)
+
+// var str = 'his car is red also his pant is red and house also red';
+
+// var change = str.replaceAll('red', 'blue');
+
+// console.log(change);
+
+// var arr1 = ['mohib', 'rehan', 'shehzad'];
+
+// console.log()
+
+// var chan = arr1.charAt(1);
+// console.log(chan);
+
+// var str = 'his car is red also his pant is red and house also red';
+
+// console.log(str.charAt(12));

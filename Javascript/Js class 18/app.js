b6e860085f9f724555ce258999952be1b3507d48
Majid Hussain <majidrajput246@gmail.com>
2025-12-29 18:52:10 +0500

    js class 18 number methods

---
+++ Javascript/Js class 18/app.js
@@ -0,0 +1,50 @@
+// number methods 
+
+// var num1 = 12.5;
+// console.log(Math.round(num1))
+
+// var num1 = 12.2;
+
+// console.log(Math.ceil(num1))
+
+// var num1 = 12.7;
+
+// console.log(Math.floor(num1))
+
+// console.log(Math.floor(Math.random() * 6 + 1))
+
+// function toss() {
+//     var tossStart = Math.floor(Math.random() * 2 + 1);
+
+//     var result = document.getElementById('result');
+    
+//     if(tossStart === 1){
+//       result.innerHTML = `Babar Win ${tossStart}`
+//     }else{
+//             result.innerHTML = `Kohli Win ${tossStart}`
+
+      
+//     }
+
+
+// }
+
+// toss();
+
+
+// var myFractional = parseFloat("1.9999");
+// console.log(myFractional);
+
+// var num1 = 1234;
+
+// console.log(num1.toString())
+
+// var total = 12.3456789;
+
+// // var prettyTotal = total.toFixed(2)
+
+// var prettyTotal = total.toFixed(4)
+
+
+// console.log(prettyTotal)
+

330caad26ab2fcf988e0e22474a1654f8052e244
Majid Hussain <majidrajput246@gmail.com>
2026-01-01 13:46:39 +0500

    js class 19 objects

---
+++ Javascript/Js class 19/app.js
@@ -0,0 +1,34 @@
+
+// var obj1 = {
+//     stdName : 'Subhan',
+//     rollNum : 12,
+//     class : 'five',
+//     age : 10 
+// }
+
+// document.write(obj1.stdName, obj1.age, obj1.class)
+// document.write(obj1)
+// console.log(obj1)
+// console.log(document)
+
+// for(var key in obj1){
+//     document.write(key + ':' + obj1[key] + "<br>");
+// }
+
+
+
+// var obj1 = {
+//     stdName : 'Subhan',
+//     rollNum : 12,
+//     class : 'five',
+//     age : 10 
+// }
+
+// var newStd = 'Saleem';
+
+// obj1.stdName = newStd;
+
+// console.log(obj1.stdName)
+
+// var name1 = document.getElementById('name1').innerHTML = 'Basic'
+

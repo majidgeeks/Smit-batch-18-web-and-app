82d655545f48cb9f964fe58b8c6c091741e92b08
Majid Hussain <majidrajput246@gmail.com>
2026-01-14 19:09:28 +0500

    js class 25

---
+++ Javascript/Js class 25/app.js
@@ -0,0 +1,61 @@
+// date and time
+
+// var date = new Date();
+// var gettingDate = date.getDate()
+// document.write(gettingDate + '<br>')
+
+// var gettingDay = date.getDay();
+// document.write(gettingDay);
+
+// var gettingHours = date.getHours()
+// document.write(gettingHours);
+
+// var str = date.toString();
+// var copyDate = str.slice(0,15)
+
+// document.write(str)
+
+// console.log(typeof str)
+
+// var dob = new Date(prompt(`write age in this (12, august 1999) format`));
+// var dobTime = dob.getTime();
+// console.log('dobTime',dobTime)
+
+// var date = new Date()
+// var dTime = date.getTime()
+// console.log('dTime',dTime)
+
+// var calAge = dTime - dobTime;
+// var final = Math.floor(calAge / (1000 * 60 * 60 * 24 * 365));
+// console.log('final',final)
+
+//========================================================================================================
+
+// switch
+
+// var userAge = 30;
+
+// switch (userAge){
+//     case 20 :
+//         document.write('age is correct')
+//         break;
+//     case 10 :
+//         document.write('age is 10')
+//     default:
+//         document.write('plz enter correct age ')
+//         break;
+// }
+
+// while loop
+
+//  var i = 0;
+//  while (i <= 3) {
+//   alert(i);
+//   i++;
+//  }
+
+// var i = 1;
+// do {
+//   alert(i);
+//   i++;
+// } while (i < 4);

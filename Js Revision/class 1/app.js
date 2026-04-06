fce4be293cd624d4cb6413c0db6b3a2ead4ab70b
Majid Hussain <majidrajput246@gmail.com>
2026-04-06 18:40:08 +0500

    revision class 1

---
+++ Js Revision/class 1/app.js
@@ -0,0 +1,111 @@
+
+// let myFav = 'Red';
+
+
+
+
+// hello();
+
+// function hello(){
+//   console.log('Hello World');
+// }
+
+
+// hello()
+// var hello = () => {
+//   console.log('Hello World');
+
+// }
+
+
+// console.log(user);
+// var user = 'Majid'
+
+// let newFunc = () => {
+//     var user = 'Saylani'
+//     console.log()
+// };
+
+// newFunc();
+
+// console.log(user);
+
+
+// console.log(myFav)
+
+
+// let test = () => {
+//     let user = 1
+//     console.log('user first',user)
+// }
+
+// test()
+
+// let test2 = () => {
+//     let user = 2
+//     console.log('user second',user)
+
+// };
+
+// test2();
+
+
+// let test3 = () => {
+//     let user = 3
+//     console.log('user third',user)
+
+// };
+
+// test3()
+
+// console.log(user)
+
+
+//===============================================================================
+
+
+// switch case 
+
+// let dayInWeek;
+// let today = new Date();
+// let days = today.getDay();
+// console.log(days)
+
+// if(days === 1){
+//     dayInWeek = 'Mon'
+// }else if(days === 2){
+//     dayInWeek = 'Tue'
+// }else if(days === 3){
+//     dayInWeek = 'Wed'
+// }else if(days === 4){
+//     dayInWeek = 'Thu'
+// }else if(days === 5){
+//     dayInWeek = 'Fri'
+// }else if(days === 6){
+//     dayInWeek = 'Sat'
+// }else{
+//     dayInWeek = 'Sun'
+// };
+
+// switch (dayInWeek) {
+//     case 'Sat':
+//         console.log('Its Holiday')
+//         break;
+//     case 'Sun':
+//         console.log('Its also holiday')  
+//         break;
+//         case 'Mon':
+//         console.log('Its Monday')  
+//         break;  
+//         case 'Tue':
+//         console.log('Its Tuesday')  
+//         break;
+//     default:
+//         console.log('So sad its working day');
+//         break;
+// }
+
+
+// ================================================================
+
+

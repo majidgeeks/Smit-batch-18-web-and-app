392477fab9ba9ff86beb02ff676aff04d6f28ffc
Majid Hussain <majidrajput246@gmail.com>
2026-04-08 18:30:08 +0500

    class 2 revision

---
+++ Js Revision/class 2/app.js
@@ -0,0 +1,39 @@
+
+// for(let i = 0; i < 5; i++){
+
+// }
+
+// while loop 
+
+// let i = 10;
+
+// while (i <= 5) {
+//     alert(i);
+//     i++;
+// }
+
+// let i = 10;
+
+// do {
+//     alert(i);
+//     i++;
+// }
+
+// while(i <= 5);
+
+// if (document.getElementById(fieldId).value === ""){
+//     alert("Email address required.");
+// }
+
+
+// document.getElementById(fieldId).value === "" ? alert('email required') : alert('form submited')
+
+// document.getElementById(fieldId).value === "" && alert('email required');
+
+// document.getElementById(fieldId).value !== "" alert('form submitted')
+
+var myName = 'majid';
+
+// myName = '';
+
+!myName  

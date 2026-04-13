b72944adcd4618d6b8ac517edb8dcf3829c91887
Majid Hussain <majidrajput246@gmail.com>
2026-04-13 18:15:07 +0500

    revision objects

---
+++ Js Revision/class 4/app.js
@@ -0,0 +1,33 @@
+// function Plan (name, price, space, transfer, pages, discountMonths) {
+//     this.name = name;
+//     this.price = price;
+//     this.space = space;
+//     this.transfer = transfer;
+//     this.pages = pages;
+//     this.discountMonths = discountMonths;
+//     this.calc = function() {
+//         console.log(this.price);
+//     }
+// };
+
+
+
+// let plan1 = new Plan('Basic', 1000, 100, 1223, 10, 0.05);
+// console.log(plan1);
+
+// plan1.calc();
+// let plan2 = new Plan('Premium', 1500, 120, 1226, 20, 0.05);
+// console.log(plan2);
+
+// let today = new Date()
+
+
+let arr = [1,2,3,4];
+
+// let calc = arr.reduce((acc, current) => acc + current );
+// console.log(calc);
+
+// let multy = arr.reduce((accumulated, current) => accumulated * current );
+// console.log(multy);
+
+ 

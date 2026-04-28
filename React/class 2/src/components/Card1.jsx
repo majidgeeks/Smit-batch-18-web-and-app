83853acced1ff748c11e612a44952d90376240b7
Majid Hussain <majidrajput246@gmail.com>
2026-04-28 21:39:11 +0500

    Jsx and create dynamic component

---
+++ React/class 2/src/components/Card1.jsx
@@ -0,0 +1,16 @@
+
+
+const Card1 = ({name, para}) => {
+    return (
+        <div>
+        <h2>{name}</h2>
+        <p>{para} </p>
+        <img
+          src="https://images.unsplash.com/photo-1761839258575-038fef381ee7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+      </div>
+    )
+};
+
+export default Card1;

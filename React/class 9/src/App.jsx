387b02d7b03895b4681af512d438b909311983c5
Majid Hussain <majidrajput246@gmail.com>
2026-05-15 18:43:38 +0500

    class 9

---
+++ React/class 9/src/App.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+// import Home from "./components/Home";
+// import Card1 from "./components/Card1";
+// import users from "./assets/data/userData";
+import DataBinding from './components/DataBinding';
+import Parent from './components/Parent';
+// import State from './components/State';
+import './index.css'
+// import CustomInput from "./components/shared/CustomInput";
+// import CompoC from "./components/CompoC";
+
+
+const App = () => {
+  
+
+  return (
+  
+    <div>
+       {/* <DataBinding /> */}
+       <Parent />
+    </div>
+  );
+};
+
+export default App;
